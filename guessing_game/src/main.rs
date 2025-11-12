f98f0ccf51508f41d94fee3b08093a99d8603a71
Maxim Yefremov <maxyefr@gmail.com>
2025-11-12 19:21:08 +0300

    show attempts count

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     println!("Welcome to guessing game, I have a number on my mind now between 1 and 100, \
     please it, and I will tell you if it is too high or too low or maybe you it is my number.");
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut attempts = 0;
     loop {
+        attempts+=1;
         println!("Please input your guess.");
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
@@ -14,7 +16,8 @@ fn main() {
         let guess: u32 = input.trim().parse().expect("Please type a number!");
         println!("{} your number", guess);
         if guess == secret_number {
-            println!("You win!");
+            println!("You win! Attempts amount: {}", attempts);
+            println!("The secret number is: {}", secret_number);
             break;
         } else if guess < secret_number {
             println!("Too low!");
