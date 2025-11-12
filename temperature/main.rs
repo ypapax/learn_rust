6c9f1a9e55938bcd6c845ac3800ce192f7a09a8f
Maxim Yefremov <maxyefr@gmail.com>
2025-11-12 16:46:13 +0300

    farenheit works

--- temperature/main.rs
+++ temperature/main.rs
@@ -1,7 +1,11 @@
 use std::io;
 fn main() {
-    let mut celcius = String::new();
+    let mut input = String::new();
     println!("input your celcius temperature:");
-    io::stdin().read_line(&mut celcius).expect("Failed to read line");
-    println!("{} Celcius inputed", celcius)
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    println!("{} Celcius inputed", input);
+    let celc: f32 = input.trim().parse().expect("Failed to parse input");
+    println!("{} Celcius parsed", celc);
+    let fahr = celc * 1.8 + 32.0;
+    println!("{} Fahrenheit", fahr);
 }
